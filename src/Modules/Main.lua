2df27c73cbf45e0980ee94564d241cc092e8bfdf
Nightblade <Nightblade@users.noreply.github.com>
2025-11-21 16:41:27 +1100

    add GetCloudProvider status to popup

--- src/Modules/Main.lua
+++ src/Modules/Main.lua
@@ -1575,14 +1575,14 @@ end
 -- Help button opens a URL to PoB's GitHub wiki.
 function main:OpenCloudErrorPopup(fileName)
 	local provider, _, status = GetCloudProvider(fileName)
-	ConPrintf('Error: file offline "%s" provider: "%s" status: "%s"',
-		fileName or "?", provider, status)
+	ConPrintf('Error: file offline "%s" provider: "%s" status: "%s"',fileName, provider, status)
 	fileName = fileName and "\n\n^8'"..fileName.."'" or ""
 	local pobVersion = "^8v"..launch.versionNumber..(launch.versionBranch == "dev" and " (Dev)" or launch.versionBranch == "beta" and " (Beta)" or "")
 	local title = " Error "
 	provider = provider or "your cloud provider"
+	local statusText = tostring(status) or "nil"
 	local msg = "\nCannot read file.\n\nMake sure "..provider.." is running then restart "..APP_NAME.." and try again."..
-		fileName.."\n\n"..pobVersion
+		fileName.."\nstatus: "..statusText.."\n\n"..pobVersion
 	local url = "https://github.com/PathOfBuildingCommunity/PathOfBuilding/wiki/CloudError"
 	local controls = { }
 	local numMsgLines = 0
